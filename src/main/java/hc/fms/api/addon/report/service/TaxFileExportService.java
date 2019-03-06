b892ec92c16e1d3b925a58ff4759c5261f8e533a
변재욱 <>
2019-03-06 21:41:35 +0900

    implement exporting excel file basic

--- src/main/java/hc/fms/api/addon/report/service/TaxFileExportService.java
+++ src/main/java/hc/fms/api/addon/report/service/TaxFileExportService.java
@@ -17,44 +17,59 @@ import hc.fms.api.addon.vhctax.entity.VehicleTaxTask;
 @Service
 public class TaxFileExportService {
 	private static String[] taxListColumnNames = { "Label", "Model", "Plate No.", "Type", "Payment No.", "Cost",
-			"Due Date", "Status", "Notification", "Photo", "Payment Receipt", "Edit/Remove" };
+			"Due Date", "Status"};
 
 	public ByteArrayInputStream exportToExcel(List<VehicleTaxTask> list) {
-		
+
 		Workbook xlsxWb = new XSSFWorkbook();
 
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		
+
 		Sheet sheet1 = xlsxWb.createSheet("FirstSheet");
-		
+
 		sheet1.setColumnWidth(0, 10000);
 		sheet1.setColumnWidth(9, 10000);
-		
+
 		Row row = null;
 		Cell cell = null;
-		
-		row = sheet1.createRow(0);
-		
-		cell = row.createCell(0);
-		cell.setCellValue("1-1");
-		
-		cell = row.createCell(1);
-		cell.setCellValue("1-2");
-		
-		row = sheet1.createRow(1);
-		cell = row.createCell(0);
-		cell.setCellValue("2-1");
-		
-		cell = row.createCell(1);
-		cell.setCellValue("2-2");
-		
-		row = sheet1.createRow(2);
-		cell = row.createCell(0);
-		cell.setCellValue("3-1");
-		
-		cell = row.createCell(1);
-		cell.setCellValue("3-2");
-		
+		for (int i = 0; i < list.size() + 1; i++) {
+			row = sheet1.createRow(i);
+			
+			for (int j = 0; j < taxListColumnNames.length; j++) {
+				cell = row.createCell(j);
+				if (i == 0) {
+					cell.setCellValue(taxListColumnNames[j]);
+				} else {
+					switch (j) {
+					case 0:
+						cell.setCellValue(list.get(i-1).getLabel());
+						break;
+					case 1:
+						cell.setCellValue(list.get(i-1).getModel());
+						break;
+					case 2:
+						cell.setCellValue(list.get(i-1).getPlateNo());
+						break;
+					case 3:
+						cell.setCellValue(list.get(i-1).getTaskType());
+						break;
+					case 4:
+						cell.setCellValue(list.get(i-1).getRegistrationNo());
+						break;
+					case 5:
+						cell.setCellValue(list.get(i-1).getCost().toString());
+						break;
+					case 6:
+						cell.setCellValue(list.get(i-1).getDateValidTill());
+						break;
+					case 7:
+						cell.setCellValue("");
+						break;
+					}
+				}
+			}
+		}
+
 		try {
 			xlsxWb.write(out);
 		} catch (IOException e) {
